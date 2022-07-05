8c31027d205e696f315c9bc5c00496a97167413f
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-07-05 19:13:13 +0530

    user details added in the page

--- client/src/pages/BookRide.jsx
+++ client/src/pages/BookRide.jsx
@@ -6,6 +6,9 @@ export const BookRide = () => {
     const driver = useSelector((state) => state.driverReducer.driver);
     console.log('driver:', driver);
 
+    const rider = useSelector((state) => state.riderReducer.rider);
+    const { firstName, lastName, coordinates } = rider;
+
 
 
     return (
@@ -14,11 +17,18 @@ export const BookRide = () => {
                 <h1 style={{ textAlign: 'center' }}>Book a Ride</h1>
 
                 <div className="border home">
+                    <h3>Rider Details</h3>
+                    <p>{firstName} {lastName} || Coordinates : ({coordinates.x}, {coordinates.y})</p>
+                </div>
+
+                <div className="border home">
                     <h3>List of Drivers available</h3>
                     {driver.map(({ firstName, lastName, coordinates, availability }, index) => (
                         <p key={index}>{firstName} {lastName} || Availability: {availability} || Coordinates: ({coordinates.x}, {coordinates.y})</p>
                     ))}
                 </div>
+
+
             </div>
 
         </>

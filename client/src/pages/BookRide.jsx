c92213f7658f37a44a6316dd74db27823ac19bca
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-07-05 19:09:42 +0530

    List of drivers added

--- client/src/pages/BookRide.jsx
+++ client/src/pages/BookRide.jsx
@@ -6,15 +6,18 @@ export const BookRide = () => {
     const driver = useSelector((state) => state.driverReducer.driver);
     console.log('driver:', driver);
 
-    
+
 
     return (
         <>
             <div>
                 <h1 style={{ textAlign: 'center' }}>Book a Ride</h1>
 
-                <div>
-
+                <div className="border home">
+                    <h3>List of Drivers available</h3>
+                    {driver.map(({ firstName, lastName, coordinates, availability }, index) => (
+                        <p key={index}>{firstName} {lastName} || Availability: {availability} || Coordinates: ({coordinates.x}, {coordinates.y})</p>
+                    ))}
                 </div>
             </div>
 

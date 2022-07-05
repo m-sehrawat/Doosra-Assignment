fd64c36d2d95afcd2dfa0191d5c5ee739bdf830c
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-07-05 16:34:06 +0530

    Rider signup added

--- client/src/pages/RiderAuth.jsx
+++ client/src/pages/RiderAuth.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+
+
 export const RiderAuth = () => {
+
+    const initState = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        mobile: "",
+        role: 'rider',
+    }
+
+    const [form, setForm] = useState(initState);
+
+    const handleInputChange = ({ target: { value, name } }) => {
+        setForm({ ...form, [name]: value })
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(form);
+    }
 
     return (
         <>
-            <h1>Rider</h1>
+            <div className="centerInput">
+                <h3>Register as a Rider</h3>
+
+                <form onSubmit={handleFormSubmit} className="form">
+                    <input onChange={handleInputChange} name="firstName" type="text" placeholder="First Name" />
+                    <input onChange={handleInputChange} name="lastName" type="text" placeholder="Last Name" />
+                    <input onChange={handleInputChange} name="email" type="email" placeholder="Email" />
+                    <input onChange={handleInputChange} name="mobile" type="tel" placeholder="Mobile" />
+                    <input type="submit" value={'Signup'} />
+                </form>
+            </div>
         </>
     );
 };

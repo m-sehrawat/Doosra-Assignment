5c3f9b082570afe647bab26e631ed9ca26da5794
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-07-05 16:57:33 +0530

    coordinates added

--- client/src/pages/DriverAuth.jsx
+++ client/src/pages/DriverAuth.jsx
@@ -11,16 +11,23 @@ export const DriverAuth = () => {
         role: 'driver',
     }
 
+
     const [form, setForm] = useState(initState);
+    const [coordinates, setCoordinates] = useState({ x: "", y: "" });
 
     const handleInputChange = ({ target: { value, name } }) => {
         setForm({ ...form, [name]: value })
     };
 
+    const handleCoordinateChange = ({ target: { value, name } }) => {
+        setCoordinates({ ...coordinates, [name]: value })
+    };
+
     const handleFormSubmit = (e) => {
         e.preventDefault();
-        console.log(form);
-    }
+        const payload = { user: form, coordinates };
+        console.log(payload);
+    };
 
     return (
         <>
@@ -32,6 +39,9 @@ export const DriverAuth = () => {
                     <input onChange={handleInputChange} name="lastName" type="text" placeholder="Last Name" />
                     <input onChange={handleInputChange} name="email" type="email" placeholder="Email" />
                     <input onChange={handleInputChange} name="mobile" type="number" placeholder="Mobile" />
+                    <p>Location (type your location coordinates)</p>
+                    <input onChange={handleCoordinateChange} name="x" type="number" placeholder="Coordinate X" />
+                    <input onChange={handleCoordinateChange} name="y" type="number" placeholder="Coordinate Y" />
                     <input type="submit" value={'Signup'} />
                 </form>
             </div>
